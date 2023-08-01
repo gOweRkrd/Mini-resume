398107e1c2bbfa8b1a5c65776b505afdf63b023d
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-08-01 10:37:44 +0300

    Added folder restructuring and custom color and R.string

---
+++ Mini resume/Resources/R.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+enum StringResources {
+    
+    enum ViewController {
+        static let title = "Профиль"
+    }
+    
+    enum Colors {
+        static let white = UIColor(hexString: "#FFFFFF")
+        static let black = UIColor(hexString: "#000000")
+    }
+}
+
+typealias R = StringResources
